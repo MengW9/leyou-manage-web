5c71746159d7ca50ee7134a940ca99c6f3ee0864
MengW9 <1776408028@qq.com>
2018-11-14 15:05:16 +0800

    two commit all.

--- src/pages/item/MyBrandForm.vue
+++ src/pages/item/MyBrandForm.vue
@@ -1,25 +1,18 @@
 <template>
   <v-form v-model="valid" ref="myBrandForm">
-    <v-text-field
-      label="品牌名称"
-      v-model="brand.name"
-      :rules="[v => !!v || '品牌名称不能为空']"
-      :counter="10"
-      required
-    />
-    <v-text-field
-      label="首字母"
-      v-model="brand.letter"
-      :rules="[v => v.length === 1 || '首字母只能是1位']"
-      required
-      mask="A"
-    />
+    <v-text-field v-model="brand.name" label="请输入品牌名称" required
+                  :rules="[v => !!v || '品牌名称不能为空']"
+                  :counter="10"/>
+    <v-text-field v-model="brand.letter" label="请输入品牌首字母"
+                  :rules="[v => v.length === 1 || '首字母只能是1位']" required mask="A"/>
+
     <v-cascader
       url="/item/category/list"
       multiple
       required
       v-model="brand.categories"
       label="请选择商品分类"/>
+
     <v-layout row>
       <v-flex xs3>
         <span style="font-size: 16px; color: #444">品牌LOGO：</span>
@@ -27,13 +20,13 @@
       <v-flex>
         <v-upload
           v-model="brand.image"
-          url="/upload"
+          url="/upload/image"
           :multiple="false"
-          :pic-width="250"
-          :pic-height="90"
+          :pic-width="250" :pic-height="90"
         />
       </v-flex>
     </v-layout>
+
     <v-layout class="my-4" row>
       <v-spacer/>
       <v-btn @click="submit" color="primary">提交</v-btn>
@@ -42,17 +35,28 @@
   </v-form>
 </template>
 
+
 <script>
   export default {
-    name: "MyBrandForm",
-    data()  {
+    name: "my-brand-form",
+    props:{
+      oldBrand:{
+        type:Object
+      },
+      isEdit:{
+        type:Boolean,
+        default:false,
+      }
+    },
+    data() {
       return {
-        valid: false, // 表单校验结果标记
-        brand: {
-          name: '', // 品牌名称
-          letter: '', // 品牌首字母
-          image: '',// 品牌logo
-          categories: [], // 品牌所属的商品分类数组
+        valid:false, // 表单校验结果标记
+        brand:{
+          name:'', // 品牌名称
+          letter:'', // 品牌首字母
+          image:'',// 品牌logo
+          categories:[], // 品牌所属的商品分类数组
+
         }
       }
     },
@@ -68,14 +72,33 @@
           // 4、将字母都处理为大写
           params.letter = letter.toUpperCase();
           // 5、将数据提交到后台
-          this.$http.post('/item/brand', this.$qs.stringify(params))
+          /*this.$http.post('/item/brand', this.$qs.stringify(params))
             .then(() => {
               // 6、弹出提示
+              this.$emit("close");
               this.$message.success("保存成功！");
+
             })
             .catch(() => {
               this.$message.error("保存失败！");
+            });*/
+
+
+          // 将数据提交到后台
+// this.$http.post('/item/brand', this.$qs.stringify(params))
+          this.$http({
+            method: this.isEdit ? 'put' : 'post', // 动态判断是POST还是PUT
+            url: '/item/brand',
+            data: this.$qs.stringify(params)
+          }).then(() => {
+            // 关闭窗口
+            this.$emit("close");
+            this.$message.success("保存成功！");
+          })
+            .catch(() => {
+              this.$message.error("保存失败！");
             });
+
         }
       },
       clear(){
@@ -83,17 +106,26 @@
         this.$refs.myBrandForm.reset();
         // 需要手动清空商品分类
         this.categories = [];
-      },
-      closeWindow(){
-        // 关闭窗口
-        this.show = false;
-        // 重新加载数据
-        this.getDataFromServer();
-      },
+      }
+    },
+    watch: {
+      oldBrand: {// 监控oldBrand的变化
+        handler(val) {
+          if(val){
+            // 注意不要直接复制，否则这边的修改会影响到父组件的数据，copy属性即可
+            this.brand =  Object.deepCopy(val)
+          }else{
+            // 为空，初始化brand
+            this.brand = {
+              name: '',
+              letter: '',
+              image: '',
+              categories: [],
+            }
+          }
+        },
+        deep: true
+      }
     },
   }
 </script>
-
-<style scoped>
-
-</style>
