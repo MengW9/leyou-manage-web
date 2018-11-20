69511d670cbed0ba5139a925f349ee62279406e2
MengW9 <1776408028@qq.com>
2018-11-20 16:51:12 +0800

    four commit all.

--- src/pages/item/MyBrandForm.vue
+++ src/pages/item/MyBrandForm.vue
@@ -39,7 +39,7 @@
     <v-layout class="my-4" row>
       <v-spacer/>
       <v-btn @click="submit" color="primary">提交</v-btn>
-      <v-btn @click="clear" >重置</v-btn>
+      <v-btn @click="clear">重置</v-btn>
     </v-layout>
   </v-form>
 </template>
@@ -48,34 +48,34 @@
 <script>
   export default {
     name: "my-brand-form",
-    props:{
-      oldBrand:{
-        type:Object
+    props: {
+      oldBrand: {
+        type: Object
       },
-      isEdit:{
-        type:Boolean,
-        default:false,
+      isEdit: {
+        type: Boolean,
+        default: false,
       }
     },
     data() {
       return {
-        valid:false, // 表单校验结果标记
-        brand:{
-          name:'', // 品牌名称
-          letter:'', // 品牌首字母
-          image:'',// 品牌logo
-          categories:[], // 品牌所属的商品分类数组
+        valid: false, // 表单校验结果标记
+        brand: {
+          name: '', // 品牌名称
+          letter: '', // 品牌首字母
+          image: '',// 品牌logo
+          categories: [], // 品牌所属的商品分类数组
 
         }
       }
     },
-    methods:{
-      submit(){
+    methods: {
+      submit() {
         // 提交表单
         // 1、表单校验
         if (this.$refs.myBrandForm.validate()) {
           // 2、定义一个请求参数对象，通过解构表达式来获取brand中的属性
-          const {categories ,letter ,...params} = this.brand;
+          const {categories, letter, ...params} = this.brand;
           // 3、数据库中只要保存分类的id即可，因此我们对categories的值进行处理,只保留id，并转为字符串
           params.cids = categories.map(c => c.id).join(",");
           // 4、将字母都处理为大写
@@ -110,7 +110,7 @@
 
         }
       },
-      clear(){
+      clear() {
         // 重置表单
         this.$refs.myBrandForm.reset();
         // 需要手动清空商品分类
@@ -120,10 +120,10 @@
     watch: {
       oldBrand: {// 监控oldBrand的变化
         handler(val) {
-          if(val){
+          if (val) {
             // 注意不要直接复制，否则这边的修改会影响到父组件的数据，copy属性即可
-            this.brand =  Object.deepCopy(val)
-          }else{
+            this.brand = Object.deepCopy(val)
+          } else {
             // 为空，初始化brand
             this.brand = {
               name: '',
